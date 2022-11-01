5c38c5dd5c50bc22dbfa8816e8812801e6b4f762
Ven Ven <esstat17@gmail.com>
2022-11-01 19:56:55 +0800

    profile omitted

--- backend.tf
+++ backend.tf
@@ -10,7 +10,6 @@
 # See terraform registry @https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
   region  = "us-west-1"
-  profile = "app-terraform"
   # shared_credentials_files = ["~/.aws/credentials"]
   # shared_config_files      = ["~/.aws/config"]
 }
